b9a5055b321a2619588857af2b627942b35fb074
Johnny Murillo <jossters@gmail.com>
2021-06-11 12:21:47 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 
-const projectRouter = require('./project/router');
-const resourceRouter = require('./resource/router');
-const taskRouter = require('./task/router');
+// const projectRouter = require('./project/router');
+// const resourceRouter = require('./resource/router');
+// const taskRouter = require('./task/router');
 
 const server = express();
 
 server.use(express.json());
 
-server.use('/api/projects', projectRouter);
-server.use('/api/routers', resourceRouter);
-server.use('/api/tasks', taskRouter);
+// server.use('/api/projects', projectRouter);
+// server.use('/api/routers', resourceRouter);
+// server.use('/api/tasks', taskRouter);
 
 server.use('*', (req, res, ) => {
     res.json({ api: 'up'})

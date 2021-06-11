a7171c128f653b77fa6c6b1ef796e39b97540ebc
Johnny Murillo <jossters@gmail.com>
2021-06-11 11:49:53 -0700

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,20 @@
-// build your `/api/projects` router here
+const router = require('express').Router()
+const Project = require('./model')
+
+router.get('/', (req, res, next) => {
+    Project.project_id(req.params.project_id)
+    .then(res => {
+        res.status(200).json(res)
+    })
+    .catch(next)
+})
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        custom: "ohhh",
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = router
